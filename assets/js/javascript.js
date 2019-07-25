f156348a36517a418a4305475974ef867f72a6da
auxfuse <Anthony.obrien2@hse.ie>
2019-07-25 22:31:43 +0100

    Javascript function added to open modal on settings click.

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,3 +1,9 @@
+//Load modal window on page load.
 $(document).ready(function(){
     $("#openingSettings").modal('show');
 });
+
+//Load modal window on "Settings" click.
+function openModal(){
+    $("#openingSettings").modal('show');
+};
