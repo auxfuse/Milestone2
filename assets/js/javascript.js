1f5517f43fb01af92d34774a37c2331e1f5c9fb0
auxfuse <Anthony.obrien2@hse.ie>
2019-08-05 11:46:53 +0100

    Removed themeSpan set from each if set on theme background to compress code

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -17,22 +17,18 @@ function changeBackground(){
     let bodyBg = document.body;
     let themeIndex = document.getElementById("theme");
     let themeValue = themeIndex.options[themeIndex.selectedIndex].value;
+    themeSpan.innerHTML = themeValue;
 
     if (themeValue === "pokemon"){
         bodyBg.classList.add("pokemon-bg");
-        themeSpan.innerHTML = themeValue;
     } else if (themeValue === "superheroes"){
         bodyBg.classList.add("comicbook-bg");
-        themeSpan.innerHTML = themeValue;
     } else if (themeValue === "cars"){
         bodyBg.classList.add("cars-bg");
-        themeSpan.innerHTML = themeValue;
     } else if (themeValue === "nature"){
         bodyBg.classList.add("nature-bg");
-        themeSpan.innerHTML = themeValue;
     } else if (themeValue === "european cities"){
         bodyBg.classList.add("city-bg");
-        themeSpan.innerHTML = themeValue;
     }
 }
 changeBackground();
