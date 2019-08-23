b8dd1877d6b5247040be71feb18a1c2dcfcd78b6
auxfuse <Anthony.obrien2@hse.ie>
2019-08-23 21:32:55 +0100

    MutationObserver used to watch modal display, if open gameboard is hidden and vice versa

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,12 +1,12 @@
 //Global Statements
 
     //Modal Elements to target via Javascript
-const modal = document.querySelector("#openingSettings");
+const modalMenu = document.querySelector("#openingSettings");
 const diffDropdown = document.querySelector("#difficulty");
 const themeDropdown = document.querySelector("#theme");
 
     //Main page Elements to target via Javascript
-const bodyBg = documument.body;
+const bodyBg = document.body;
 const gameboard = document.querySelector(".game-board");
 const themeSpan = document.querySelector("#theme-span");
 const diffSpan = document.querySelector("#diff-span");
@@ -14,3 +14,13 @@ const shownWord = document.querySelector("#shown-word");
 const timeSpan = document.querySelector("#time-span");
 const userInput = document.querySelector("#user-input");
 const scoreSpan = document.querySelector("#score-span");
+
+//Modal DOM change to hide or show gameboard
+const observer = new MutationObserver(() => {
+    if (modalMenu.style.display == "block"){
+        gameboard.style.display = "none"
+    } else {
+        gameboard.style.display = "block"
+    }
+});
+observer.observe(modalMenu, { attributes: true, childList: true});
